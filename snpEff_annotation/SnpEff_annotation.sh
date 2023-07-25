f794a39117c10b47bc28dcf3fcaaad1090943536
Hanslab <140466030+zlguu@users.noreply.github.com>
2023-07-25 14:59:17 +0800

    Delete SnpEff_annotation.sh

--- snpEff_annotation/SnpEff_annotation.sh
+++
@@ -1,5 +0,0 @@
-##Snpeff_annotation
-java -jar snpEff.jar -i vcf -o vcf Oryza_sativa a.vcf >a_snpeff.vcf
-##Extract_annotation_information
-perl snpeff_annotation_extract.pl a_snpeff.vcf >a_snpeff.annotation
-##the_format_of_outputfile: chromosome\tcoordination\tAllele1\tthe_potential_effect_of_allele1\tAllele2\tthe_potential_effect_of_allele2......
